fe89bd80abcce094c140de98113bf201f9871c35
Bruno Marcenaro <bems2588@gmail.com>
2022-04-27 13:59:30 -0400

    add intern

--- index.js
+++ index.js
@@ -43,6 +43,72 @@ function acceptManager() {
     </div>
   </div>`
     console.log(employeeHTML);
+    addTeamMember();
+  });
+};
+
+acceptManager();
+
+
+function addTeamMember() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "options",
+      message: "Enter position",
+      choices: ["Add Intern", "Add Engineer", "Exit App"]
+    }
+  ]).then(({ options }) => {
+    switch (options) {
+      case "Add Intern":
+        addIntern();
+        break;
+      case "Add Engineer":
+        addEngineer(); break;
+      default:
+        exitapp()
+
+    }
+  })
+}
+
+function acceptIntern() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter Intern name: ",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "Enter Intern ID: ",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "Enter Intern email: ",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "Enter Intern school: ",
+      name: "school"
+    },
+  ]).then(({ name, id, email, school }) => {
+    const newIntern = new Intern(name, id, email, school);
+    employeeHTML += `<div class="card" style="width: 18rem;">
+    <div class="text-center bg-primary">
+    <h3>${newIntern.name}</h3>
+    <p>Intern</p>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">ID: ${newIntern.id}</h5>
+      <p class="card-text">School ${newIntern.school}</p>
+      <a href="mailto:${newIntern.email}" class="btn btn-primary">Email: ${newIntern.email}</a>
+    </div>
+  </div>`
+    console.log(employeeHTML);
+    addTeamMember();
   });
 };
 
