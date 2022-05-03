9bbcbe451f3c0716afcde5fc60eecda4203539aa
Bruno Marcenaro <bems2588@gmail.com>
2022-05-03 12:29:23 -0400

    add manager test

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -7,35 +7,35 @@ describe("Testing Manager class", () => {
     expect(typeof (myManager)).toBe("object")
   })
   it("Testing Manager Name", () => {
-    const myManager = new Employee("Bruno")
+    const myManager = new Manager("Bruno")
     expect(myManager.name).toBe("Bruno")
   })
-  it("Testing Employee ID", () => {
-    const myManager = new Employee("Bruno", 1234)
+  it("Testing Manager ID", () => {
+    const myManager = new Manager("Bruno", 1234)
     expect(myManager.id).toBe(1234)
   })
-  it("Testing Employee Email", () => {
-    const myManager = new Employee("Bruno", 1234, "test@email.com")
+  it("Testing Manager Email", () => {
+    const myManager = new Manager("Bruno", 1234, "test@email.com")
     expect(myManager.email).toBe("test@email.com")
   })
-  it("Testing Employee getName", () => {
-    const myManager = new Employee("Bruno", 1234, "test@email.com")
+  it("Testing Manager getName", () => {
+    const myManager = new Manager("Bruno", 1234, "test@email.com")
     expect(myManager.getName()).toBe("Bruno")
   })
-  it("Testing Employee getEmail", () => {
-    const myManager = new Employee("Bruno", 1234, "test@email.com")
+  it("Testing Manager getEmail", () => {
+    const myManager = new Manager("Bruno", 1234, "test@email.com")
     expect(myManager.getEmail()).toBe("test@email.com")
   })
-  it("Testing Employee getId", () => {
-    const myManager = new Employee("Bruno", 1234, "test@email.com")
+  it("Testing Manager getId", () => {
+    const myManager = new Manager("Bruno", 1234, "test@email.com")
     expect(myManager.getId()).toBe(1234)
   })
-  it("Testing Employee getRole", () => {
-    const myManager = new Employee("Bruno", 1234, "test@email.com")
+  it("Testing Manager getRole", () => {
+    const myManager = new Manager("Bruno", 1234, "test@email.com")
     expect(myManager.getRole()).toBe("Manager")
   })
-  it("Testing Employee getOfficeNumber", () => {
-    const myManager = new Employee("Bruno", 1234, "test@email.com", 456)
+  it("Testing Manager getOfficeNumber", () => {
+    const myManager = new Manager("Bruno", 1234, "test@email.com", 456)
     expect(myManager.getOfficeNumber()).toBe(456)
   })
 
